0321964bfb8367cf0b84998df8830ade4986d306
Antosha Baboshin <antosha.js@gmail.com>
2022-07-02 14:57:47 +0300

    fix: update news.ts with types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,28 @@
-interface Source {
-    id: string;
-    name: string;
-    description: string;
-    url: string;
-    category: string;
-    language: string;
-    country: string;
-} 
-
-interface ResponseObject {
-    status: string;
-    sources: Source[];
+export interface Source {
+  id: string;
+  name: string;
+//   description: string;
+//   url: string;
+//   category: string;
+//   language: string;
+//   country: string;
+}
+// export interface ResponseObject {
+//   status: string;
+//   sources: Source[];
+// }
+export interface Data {
+  status: string;
+  totalResults: number;
+  articles: News[];
+}
+export interface News {
+  source: Source;
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
 }
